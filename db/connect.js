b4db06b1ce5aa425d2dbde39fa9d629ae2363f32
Hisashin7331 <hiszaszin@gmail.com>
2021-04-16 08:01:37 +0200

    Added console log with DB Connected status

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(
+    process.env.DB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => console.log('DB Connected...'),
+)
