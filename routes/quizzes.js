63909fcec78460c5ae1ec54d968978b693b8ad69
Hisashin7331 <hiszaszin@gmail.com>
2021-05-03 17:20:25 +0200

    Fixed routes, ready to deploy

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -106,7 +106,80 @@ router.get('/getQuiz', async (req, res) => {
 
 router.put('/incrementViews', (req, res) => {
     const { id } = req.body
-    Quiz.findByIdAndUpdate(id, { $inc: { views: 1 } })
+    Quiz.findByIdAndUpdate(id, { $inc: { views: 1 } }).then(() => {
+        return
+    })
+})
+
+router.get('/search', (req, res) => {
+    const { query, limit, skip } = req.query
+    if (query.length < 3) {
+        Quiz.find({})
+            .sort({ views: -1 })
+            .skip(parseInt(skip))
+            .limit(parseInt(limit))
+            .then(async quizzes => {
+                const modifiedQuizzes = await Promise.all(
+                    quizzes.map(async item => {
+                        const { username } = await User.findById(
+                            item.author,
+                        )
+                        item.author = username
+                        return item
+                    }),
+                )
+                res.json(modifiedQuizzes)
+            })
+            .catch(() =>
+                res.json({ error: 'Unexpected server error' }),
+            )
+        return
+    }
+
+    Quiz.aggregate([
+        {
+            $search: {
+                text: {
+                    query,
+                    path: ['name', 'category'],
+                },
+            },
+        },
+        {
+            $project: {
+                views: 1,
+                author: 1,
+                thumbnail: 1,
+                name: 1,
+                category: 1,
+                data: 1,
+                date: 1,
+                score: {
+                    $meta: 'searchScore',
+                },
+            },
+        },
+        {
+            $sort: {
+                score: -1,
+            },
+        },
+    ])
+        .skip(skip ? parseInt(skip) : 0)
+        .limit(limit ? parseInt(limit) : 16)
+        .then(async quizzes => {
+            const modifiedQuizzes = await Promise.all(
+                quizzes.map(async item => {
+                    const { username } = await User.findById(
+                        item.author,
+                    )
+                    item.author = username
+                    return item
+                }),
+            )
+            res.json(modifiedQuizzes)
+        })
+        .catch(() => res.json({ error: 'Unexpected server error' }))
 })
 
 module.exports = router
